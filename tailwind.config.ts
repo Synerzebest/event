91912351b189965c0c4b21736eef0e9b3cc5d955
Charles Guelton <charlesguelton07@gmail.com>
2025-01-19 20:01:46 +0100

    Fix tailwind module

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,8 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/react/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@nextui-org/react/**/*.{js,ts,jsx,tsx}",
+    './app/[lng]/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
