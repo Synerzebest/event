c0c67481811367d0e43e803a05ab72afefec69e8
Charles Guelton <charlesguelton07@gmail.com>
2025-02-07 08:54:32 +0100

    Cta button animation

--- components/CtaButton.tsx
+++ components/CtaButton.tsx
@@ -1,9 +1,12 @@
+"use client"
+
 import React from 'react';
 import Link from 'next/link';
 import { Button } from "antd";
 import { useTranslation } from "../app/i18n";
 import useLanguage from '@/lib/useLanguage';
 import { FaRegCalendarCheck } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const CtaButton = () => {
     const lng = useLanguage();
@@ -13,7 +16,20 @@ const CtaButton = () => {
         <Link href={`/${lng}/eventlab`} className="fixed bottom-4 right-4 z-20">
             <Button type="primary" size="large" className="flex items-center shadow-xl font-bold hover:scale-105 transition-transform duration-200">
                 {t('cta_button')}
-                <FaRegCalendarCheck />
+                <motion.div
+                    animate={{
+                        scale: [1, 1.2, 1],
+                        rotate: [0, -10, 10, -5, 5, 0],
+                    }}
+                    transition={{
+                        repeat: Infinity, 
+                        repeatDelay: 5, 
+                        duration: 1,
+                        ease: "easeInOut",
+                    }}
+                >
+                    <FaRegCalendarCheck />
+                </motion.div>
             </Button>
         </Link>
     )
