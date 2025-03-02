679778b64c246db4598a55ab7ebbd6b4ce3b9c7e
Charles Guelton <charlesguelton07@gmail.com>
2025-03-02 16:27:29 +0100

    Hero image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -44,8 +44,9 @@ const Hero = () => {
       {/* Overlay sombre */}
       <div className="absolute inset-0 bg-black opacity-50"></div>
 
-      <div className="relative top-12 z-10 text-center p-4 sm:p-8 flex flex-col justify-center items-center gap-4 lg:text-left">
-        <h1 className="text-4xl text-white text-center sm:text-5xl md:text-6xl font-bold">
+      <div className="relative top-24 sm:top-12 z-10 text-center p-4 sm:p-8 flex flex-col justify-center items-center gap-4 lg:text-left">
+      <h1 className="text-4xl text-white text-center sm:text-5xl md:text-6xl font-bold w-full break-words">
+
           <span className="inline">
             <FlipWords />
           </span>
@@ -54,14 +55,14 @@ const Hero = () => {
 
         <p className="text-lg text-center sm:text-xl md:text-2xl text-gray-300">{safeTranslate(t,"hero_subtitle")}</p>
 
-        <div className="mt-24 flex flex-col sm:flex-row items-center gap-4">
+        <div className="mt-36 sm:mt-24 flex flex-col sm:flex-row items-center gap-4">
         <Link href={`/${lng}/explore`}>
-            <button className="flex items-center gap-2 text-xl text-white bg-indigo-500 bg-opacity-80 backdrop-blur-md border border-[rgba(255,255,255,0.3)] py-4 px-6 font-bold rounded-xl hover:bg-indigo-600 duration-300 shadow-lg">
+            <button className="flex items-center gap-2 text-xl text-white bg-transparent bg-opacity-80 backdrop-blur-md border border-[rgba(255,255,255,0.3)] py-4 px-6 font-bold rounded-xl hover:bg-indigo-600 duration-300 shadow-lg">
               {safeTranslate(t,"hero_button")}
             </button>
           </Link>
           <Link href={`/${lng}/eventlab`}>
-            <button className="flex items-center gap-2 text-xl text-white bg-amber-500 bg-opacity-80 backdrop-blur-md border border-[rgba(255,255,255,0.3)] py-4 px-6 font-bold rounded-xl hover:bg-amber-600 duration-300 shadow-lg">
+            <button className="flex items-center gap-2 text-xl text-white bg-transparent bg-opacity-80 backdrop-blur-md border border-[rgba(255,255,255,0.3)] py-4 px-6 font-bold rounded-xl hover:bg-amber-600 duration-300 shadow-lg">
               {safeTranslate(t,"cta_button")}
             </button>
           </Link>
